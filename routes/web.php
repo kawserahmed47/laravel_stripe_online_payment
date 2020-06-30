71e5294831398d147a5b154ea3b8c658d838d61b
Kawser Ahmed <kawserahmed47@gmail.com>
2020-06-30 19:53:32 +0600

    stripe online payment

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::get('/','HomeController@index')->name('index');
+Route::get('/stripe', 'HomeController@index');
+
+Route::post('stripe', 'HomeController@stripePost')->name('stripe.post');
